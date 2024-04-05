7994ef8dfea17367275fb10c8a33c45afa169ebf
Samagra Agrawal <samagra110@gmail.com>
2024-04-05 15:50:21 +0530

    Incorporate Prebuilt Alert component and state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import React from "react";
+import { React, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Home from "./components/Home";
+import Alert from "./components/Alert";
 import NoteState from "./context/NoteState";
 
 export default function App() {
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type) => {
+    const obj = {
+      message: message,
+      type: type,
+    };
+    setAlert(obj);
+
+    //display Alert for only some seconds and then hide it(not close it)
+    //for hiding it we set it to NULL
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
   return (
     <>
       <NoteState>
         <Router>
           <Navbar />
+          <Alert></Alert>
           <div className="container">
             <Routes>
               <Route exact path="/" element={<Home />} />
