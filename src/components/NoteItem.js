ef792c07b031bf30b406e6f888b5f78a529ad966
Samagra Agrawal <samagra110@gmail.com>
2024-04-05 15:41:08 +0530

    Add fontawesome icons to note component

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -6,8 +6,20 @@ export default function NoteItem(props) {
     <div className="col-md-3">
       <div className="card my-2">
         <div className="card-body">
-          <h5 className="card-title">{note.title}</h5>
-          <p className="card-text">{note.description} Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium inventore quidem eos, autem saepe vero ea accusamus facilis sapiente deserunt eius totam laboriosam. Expedita similique dolor, quasi mollitia accusamus voluptatibus voluptates eius. Odio, dignissimos.</p>
+          <div className="d-flex align-items-center justify-content-between">
+            <h5 className="card-title">{note.title}</h5>
+            <div class="d-flex justify-content-between">
+              <i className="fa-sharp fa-solid fa-trash mx-2"></i>
+              <i className="fa-solid fa-pencil mx-2"></i>
+            </div>
+          </div>
+          <p className="card-text">
+            {note.description} Lorem ipsum, dolor sit amet consectetur
+            adipisicing elit. Praesentium inventore quidem eos, autem saepe vero
+            ea accusamus facilis sapiente deserunt eius totam laboriosam.
+            Expedita similique dolor, quasi mollitia accusamus voluptatibus
+            voluptates eius. Odio, dignissimos.
+          </p>
         </div>
       </div>
     </div>
