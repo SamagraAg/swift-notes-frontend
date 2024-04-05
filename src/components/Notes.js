7993d451ec1cc5c46d8045ca241e4fdec04292d5
Samagra Agrawal <samagra110@gmail.com>
2024-04-05 17:35:02 +0530

    New component AddNote

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -6,10 +6,13 @@ export default function Notes() {
   const context = useContext(NoteContext);
   const { notes, setNotes } = context;
   return (
-    <div className="row">
-      {notes.map((note) => {
-        return <NoteItem key={note._id} note={note}></NoteItem>;
-      })}
+    <div className="container">
+      <h2>Your Notes</h2>
+      <div className="row">
+        {notes.map((note) => {
+          return <NoteItem key={note._id} note={note}></NoteItem>;
+        })}
+      </div>
     </div>
   );
 }
