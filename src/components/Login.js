8b84fe2fec4fc929a013c32ff3ea25dc047b6a58
Samagra Agrawal <samagra110@gmail.com>
2024-04-07 13:11:12 +0530

    Authenticating user using login endpoint

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 function Login() {
+  const host = "http://localhost:5000";
+  const navigate = useNavigate();
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: "",
+  });
+  const handleClick = async (e) => {
+    e.preventDefault();
+    const response = await fetch(`${host}/api/auth/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: credentials.email,
+        password: credentials.password,
+      }),
+    });
+    const jsonResponse = await response.json();
+    if (jsonResponse.success === true) {
+      console.log(jsonResponse.authToken);
+      localStorage.setItem("token", jsonResponse.authToken);
+      navigate("/");
+    } else {
+      alert("Use correct id password");
+    }
+  };
+
+  const onChange = (e) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
   return (
     <div id="login">
       <h3 className="text-center text-light pt-5 d-none">Login form</h3>
@@ -12,30 +43,30 @@ function Login() {
         >
           <div id="login-column" className="col-md-6">
             <div id="login-box" className="col-md-12">
-              <form id="login-form" className="form" action="" method="post">
+              <form id="login-form" className="form" onSubmit={handleClick}>
                 <h3 className="text-center text-warning">Login</h3>
                 <div className="form-group my-2">
                   <label htmlFor="email" className="text-light">
                     Email:
                   </label>
-                  <br />
                   <input
                     type="email"
                     name="email"
                     id="email"
                     className="form-control"
+                    onChange={onChange}
                   />
                 </div>
                 <div className="form-group my-2">
                   <label htmlFor="password" className="text-light">
                     Password:
                   </label>
-                  <br />
                   <input
                     type="text"
                     name="password"
                     id="password"
                     className="form-control"
+                    onChange={onChange}
                   />
                 </div>
                 <div className="form-group my-2">
