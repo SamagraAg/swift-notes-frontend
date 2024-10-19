cfe8bc2f7b8eab35ddb330e9fb277ed7b429f3f6
Samagra Agrawal <114285524+SamagraAg@users.noreply.github.com>
2024-10-19 21:01:18 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,4 @@
 import { React} from "react";
 export default function About() {
-  return  <div style={{"font-family":"cursive"; "color" : "yellow"}}>The Swift Notes APP is a web application built using the MERN stack (MongoDB, ExpressJS, ReactJS, and NodeJS) that allows users to create, read, update, and delete notes. The application provides a simple and intuitive interface for users to organize their notes and quickly access them when needed.</div>;
+  return  <div style={{"font-family":"cursive"}}>The Swift Notes APP is a web application built using the MERN stack (MongoDB, ExpressJS, ReactJS, and NodeJS) that allows users to create, read, update, and delete notes. The application provides a simple and intuitive interface for users to organize their notes and quickly access them when needed.</div>;
 }
